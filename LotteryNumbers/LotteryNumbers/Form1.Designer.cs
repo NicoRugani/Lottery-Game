a419d9ff9e2e80857ca3a78e75234aab7d038f65
NicoRugani <159579130+NicoRugani@users.noreply.github.com>
2024-03-05 13:11:42 -0600

    Add files via upload

---
+++ LotteryNumbers/LotteryNumbers/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace LotteryNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPlayerNum = new System.Windows.Forms.TextBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtWinningNums = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Your Numbers:";
+            // 
+            // txtPlayerNum
+            // 
+            this.txtPlayerNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlayerNum.Location = new System.Drawing.Point(221, 94);
+            this.txtPlayerNum.Name = "txtPlayerNum";
+            this.txtPlayerNum.ReadOnly = true;
+            this.txtPlayerNum.Size = new System.Drawing.Size(150, 29);
+            this.txtPlayerNum.TabIndex = 1;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.Location = new System.Drawing.Point(96, 12);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(184, 63);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Play!";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Winning Numbers:";
+            // 
+            // txtWinningNums
+            // 
+            this.txtWinningNums.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWinningNums.Location = new System.Drawing.Point(221, 145);
+            this.txtWinningNums.Name = "txtWinningNums";
+            this.txtWinningNums.ReadOnly = true;
+            this.txtWinningNums.Size = new System.Drawing.Size(150, 29);
+            this.txtWinningNums.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(113, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 42);
+            this.label3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(115, 334);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 29);
+            this.label4.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1;
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 428);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtWinningNums);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.txtPlayerNum);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPlayerNum;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtWinningNums;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ LotteryNumbers/LotteryNumbers/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace LotteryNumbers
+{
+    public partial class Form1 : Form
+    {
+        int money = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int[] playerNumbers = new int[5];
+            int[] powerBallNumbers = new int[5];
+            string pNums = "";
+            string winningNums = "";
+            string moneySpent = "";
+
+            money -= 2;
+            moneySpent = moneySpent + " " + money.ToString();
+
+
+            HashSet<int> generatedNumbers = new HashSet<int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                int playerNum = random.Next(1, 60);
+
+                // Ensure playerNum is unique
+                while (generatedNumbers.Contains(playerNum))
+                {
+                    playerNum = random.Next(1, 60);
+                }
+
+                playerNumbers[i] = playerNum;
+                generatedNumbers.Add(playerNum);
+
+                int powerBallNum = random.Next(1, 60);
+
+                // Ensure powerBallNum is unique
+                while (generatedNumbers.Contains(powerBallNum))
+                {
+                    powerBallNum = random.Next(1, 60);
+                }
+
+                powerBallNumbers[i] = powerBallNum;
+                generatedNumbers.Add(powerBallNum);
+            }
+
+            Array.Sort(playerNumbers);
+            Array.Sort(powerBallNumbers);
+
+            for (int i = 0; i < playerNumbers.Length; i++)
+            {
+                pNums = pNums + " " + playerNumbers[i].ToString() + " ";
+                winningNums = winningNums + " " + powerBallNumbers[i].ToString() + " ";
+            }
+
+
+            txtPlayerNum.Text = pNums;
+            txtWinningNums.Text = winningNums;
+
+            if (playerNumbers == powerBallNumbers)
+            {
+                label3.Text = "You Win!";
+                money += 1000000;
+            }
+            else
+            {
+                label3.Text = "You Lose";
+            }
+
+            label4.Text = "Money spent: $" + moneySpent;
+
+        }
+
+        
+    }
+}
